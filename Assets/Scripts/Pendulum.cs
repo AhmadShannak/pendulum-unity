996902bb059fad735426738d4c5b959d8a79a5b2
AhmadShannak <shannak.ahmad96@gmail.com>
2020-03-12 01:20:02 +0200

    added a pendulum

---
+++ Assets/Scripts/Pendulum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour {
+  LineRenderer pendulumRobe;
+
+  private void Start() {
+    pendulumRobe = this.GetComponent<LineRenderer>();
+    
+  }
+
+  private void Update() {
+    pendulumRobe.SetPosition(0, this.transform.position);
+  }
+}
